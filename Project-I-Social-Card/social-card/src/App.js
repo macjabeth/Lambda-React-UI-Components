4dfa703fd4718ae0b05e3dc99c4dbfb646890d8a
macjabeth <macbethjonathan@gmail.com>
2019-02-12 19:27:00 +0000

    some structure cleanup

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
-import Footer from './components/FooterComponents/Footer';
+import Footer from './components/FooterComponents/FooterContainer';
 import './App.css';
 
 const App = () => {
   return (
     <div className='grid-container'>
       <HeaderContainer />
-      <div className="card">
-        <CardContainer />
-      </div>
-      <Footer />
+      <CardContainer />
+      <FooterContainer />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -5,11 +5,13 @@ import './Card.css';
 
 const CardContainer = () => {
   return (
-    <div className="card-container">
-      <a href="https://www.reactjs.org">
-        <CardBanner />
-      </a>
-      <CardContent />
+    <div className="card">
+      <div className="card-container">
+        <a href="https://www.reactjs.org" target="_blank">
+          <CardBanner />
+        </a>
+        <CardContent />
+      </div>
     </div>
   )
 }
