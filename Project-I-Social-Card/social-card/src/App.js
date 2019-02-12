a0b7c6fe6d1424d79dbb6f71e231d855615dc25d
macjabeth <macbethjonathan@gmail.com>
2019-02-12 00:22:36 +0000

    begin stretch: footer component layout

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import CardContainer from './components/CardComponents/CardContainer'
+import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 import './App.css';
 
 const App = () => {
   return (
     <div className='grid-container'>
-      <div className="header">
-        <HeaderContainer />
-      </div>
+      <HeaderContainer />
       <div className="card">
         <CardContainer />
       </div>
-      <div className="footer"></div>
+      <Footer />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './Footer.css';
 
-<footer className='footer'>
-  <img src=''/>
-  <img src=''/>
-  <img src=''/>
-  <img src=''/>
-</footer>
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <a><i className='far fa-comment'></i></a>
+      <a><i className="fas fa-sync-alt"></i> <span>6</span></a>
+      <a><i className="far fa-heart"></i> <span>4</span></a>
+      <a><i className="far fa-envelope"></i></a>
+    </div>
+  )
+}
 
-
-
-export default HeaderContainer;
+export default Footer;
